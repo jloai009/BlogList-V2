eaed771ec5b6efbeeb522b23d6574c688adf3174
Jose <jloai009@fiu.edu>
2022-02-13 16:49:27 -0500

    Add services

---
+++ frontend/src/services/blogs.js
@@ -0,0 +1,70 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  try {
+    const response = await axios.post(baseUrl, newObject, config)
+    return response
+  } catch (error) {
+    return {
+      message: 'Error: Title and URL are required',
+      status: 400
+    }
+  }
+
+}
+
+const put = async (id, newObject) => {
+
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const url = baseUrl + '/' + id
+
+  try {
+    const response = await axios.put(url, newObject, config)
+    return response.data
+  } catch (error) {
+    console.error('Error updating blog with id ' + id)
+  }
+}
+
+const _delete = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const url = baseUrl + '/' + id
+
+  try {
+    const response = await axios.delete(url, config)
+    return response
+  } catch (error) {
+    console.error('Error deleting blog with id ' + id)
+  }
+}
+
+// eslint-disable-next-line
+export default {
+  getAll,
+  setToken,
+  create,
+  put,
+  _delete
+}
